7dca279d827028011c7b77c1b29f0001e0abfe0a
radunicula <radu7.nicula@gmail.com>
2023-01-13 20:20:02 +0200

    finished task 4

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 class Restaurant:
-    def __init__(self, restaurant_name, cuisine_type):
+    def __init__(self, restaurant_name, cuisine_type, number_served=0):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = number_served
 
     def describe_restaurant(self):
         print(f"restaurant_name: {self.restaurant_name}")
@@ -9,6 +10,12 @@ class Restaurant:
 
     def open_restaurant(self):
         print(f"the restaurant {self.restaurant_name} is open!")
+
+    def set_number_served(self, number):
+        self.number_served = number
+
+    def increment_number_served(self, number):
+        self.number_served += number
 
 
 class User:
@@ -24,15 +31,23 @@ class User:
         print(f'Hello {self.first_name} you are {self.profile}!')
 
 
-john = User('John', 'default')
-anne = User('Anne', 'premium')
+# john = User('John', 'default')
+# anne = User('Anne', 'premium')
+#
+# john.describe_user()
+# john.greet_user()
+#
+# anne.describe_user()
+# anne.greet_user()
 
-john.describe_user()
-john.greet_user()
-
-anne.describe_user()
-anne.greet_user()
-
-# rustic = Restaurant("Rustic", "Vegetarian")
+rustic = Restaurant("Rustic", "Vegetarian")
 # taverna = Restaurant("Taverna", "General")
 # subway = Restaurant("Subway", "Fastfood")
+
+print(rustic.number_served)
+rustic.number_served = 12
+print(rustic.number_served)
+rustic.set_number_served(10)
+print(rustic.number_served)
+rustic.increment_number_served(3)
+print(rustic.number_served)
