6cefee0c07fcfaa50f8a9b3b842c88e106bcc60e
radunicula <radu7.nicula@gmail.com>
2023-01-13 20:55:28 +0200

    finished task 6

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class Restaurant:
-    def __init__(self, restaurant_name, cuisine_type, number_served=0):
+    def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
-        self.number_served = number_served
+        self.number_served = 0
 
     def describe_restaurant(self):
         print(f"restaurant_name: {self.restaurant_name}")
@@ -18,11 +18,19 @@ class Restaurant:
         self.number_served += number
 
 
+class IceCreamStand(Restaurant):
+    def __init__(self, restaurant_name, cuisine_type="Ice Cream"):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = ["Chocolate", "Vanilla", "Strawberry"]
+
+    def display_flavors(self):
+        print(self.flavors)
+
 class User:
-    def __init__(self, first_name, profile, login_attempts=0):
+    def __init__(self, first_name, profile):
         self.first_name = first_name
         self.profile = profile
-        self.login_attempts = login_attempts
+        self.login_attempts = 0
 
     def describe_user(self):
         print(f'user name: {self.first_name}')
@@ -38,35 +46,5 @@ class User:
         self.login_attempts = 0
 
 
-john = User('John', 'default')
-# anne = User('Anne', 'premium')
-#
-# john.describe_user()
-# john.greet_user()
-#
-# anne.describe_user()
-# anne.greet_user()
-
-# rustic = Restaurant("Rustic", "Vegetarian")
-# # taverna = Restaurant("Taverna", "General")
-# # subway = Restaurant("Subway", "Fastfood")
-#
-# print(rustic.number_served)
-# rustic.number_served = 12
-# print(rustic.number_served)
-# rustic.set_number_served(10)
-# print(rustic.number_served)
-# rustic.increment_number_served()
-# print(rustic.number_served)
-
-
-print(john.login_attempts)
-john.increment_login_attempts()
-print(john.login_attempts)
-john.increment_login_attempts()
-john.increment_login_attempts()
-print(john.login_attempts)
-john.reset_login_attempts()
-print(john.login_attempts)
-
-
+ice_cream_shop = IceCreamStand("Ice Cream Shop")
+print(ice_cream_shop.flavors)
