91bc6e3c3117419d28e7294e3dc2733af484d633
radunicula <radu7.nicula@gmail.com>
2023-01-15 18:00:53 +0200

    finished task 15

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import random
 
-series = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e"]
+series = ["0", "1", "2", "3", '4', '5', '6', '7', '8', '9', "a", "b", "c", "d", "e"]
 winner_ticket = []
-for i in range(4):
-    winner_ticket.append(random.choice(series))
-print(f"the winner ticket is {winner_ticket} \n "
-      f"any ticket matching these 4 numbers or letters wins a prize")
+# for i in range(4):
+#     winner_ticket.append(random.choice(series))
+# print(f"the winner ticket is {winner_ticket} \n "
+#       f"any ticket matching these 4 numbers or letters wins a prize")
+
+my_ticket = ["a", "b", "c", "d"]
+counter = 0
+
+
+while my_ticket != winner_ticket:
+    winner_ticket = []
+    for i in range(4):
+        winner_ticket.append(random.choice(series))
+    counter += 1
+
+print(f"You win after {counter} pools!")
