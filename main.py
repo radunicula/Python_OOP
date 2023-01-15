3b83c0fb31ff5799ba2dca0c07d2ead8bde458ba
radunicula <radu7.nicula@gmail.com>
2023-01-15 17:06:58 +0200

    finished task 14

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import random
 
-
+series = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e"]
+winner_ticket = []
+for i in range(4):
+    winner_ticket.append(random.choice(series))
+print(f"the winner ticket is {winner_ticket} \n "
+      f"any ticket matching these 4 numbers or letters wins a prize")
